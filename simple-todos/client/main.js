b19cd17be04a3d2ceea299a2db4dfef891f5c021
mblaul <mattblaul@gmail.com>
2018-08-07 21:52:11 -0400

    Working on using the builtin in MongoDB to display todos. Tutorial is misleading on how to do that.

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import "./main.html";
+import "../imports/api/tasks.js";
 
 import App from "../imports/ui/App.js";
 
